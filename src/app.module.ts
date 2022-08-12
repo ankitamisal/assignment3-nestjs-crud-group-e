3eb0841b14406b9d6aa3bcfd195ea2fad17607ab
vishal <vishalshinde00126@gmail.com>
2022-08-12 21:15:04 +0530

    feat:install pg database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductManagementModule } from './product-management/product-management.module';
 
 import { StudentManagmentModule } from './student-managment/student-managment.module';
 
 @Module({
-  imports: [StudentManagmentModule],
+  imports: [StudentManagmentModule,ProductManagementModule],
   controllers: [AppController],
-
-import { ProductManagementModule } from './product-management/product-management.module';
-
-
-
-
-
-@Module({
-  imports: [ProductManagementModule],
-  controllers: [AppController,],
-
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
+
+
+
+  
