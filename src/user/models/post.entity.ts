01578d416da5df6daea7f3687c10f4755dd2346b
Prathamesh094 <prathamesh.howale@gmail.com>
2022-08-15 20:21:39 +0530

    att attributes for class userpostentity

--- src/user/models/post.entity.ts
+++ src/user/models/post.entity.ts
@@ -1,5 +1,14 @@
-import {Entity} from 'typeorm';
+import {Column,Entity,PrimaryGeneratedColumn} from 'typeorm';
 @Entity('User-Post')
 export class UserPostEntity{
+    
 
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ default:''})
+    body:string;
+
+    @Column({type: 'timestamp', default:()=>'CURRENT_TIMESTAMP'})
+    createdAt: Date;
 }
