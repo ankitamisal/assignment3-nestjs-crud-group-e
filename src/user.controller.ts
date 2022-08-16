eb2e6f2417ab9012ffe970e3f9f597b085a52de8
Prathamesh094 <prathamesh.howale@gmail.com>
2022-08-16 16:57:45 +0530

    feat: add delete module

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,18 +1,37 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserPost } from './user/models/post.interface';
 import { UserService } from './user.service';
 import { Observable } from 'rxjs';
+import{ UpdateResult } from 'typeorm';
+import{ DeleteResult } from 'typeorm';
 
 @Controller('user')
 export class UserController {
 constructor(private userService: UserService) {}
 
 @Post()
-create(@Body() post: UserPost):Observable<UserPost>{
-    return this.userService.createuser(post)
+create(@Body() userPost: UserPost):Observable<UserPost>{
+    return this.userService.createuser(userPost)
 }
 @Get()
 findAll() :Observable <UserPost[]> {
     return this.userService.findAllUser();
 }
+
+@Patch(':id')
+update(
+    @Param('id') id:number,
+    @Body() userPost: UserPost,
+):Observable<UpdateResult> {
+    return this.userService.updateUser(id, userPost)
+}
+@Delete(':id')
+delete(
+    @Param('id') id:number,
+    
+):Observable<DeleteResult>{
+    return this.userService.deleteUser(id)
+    
+}
+
 }
--- src/user.service.ts
+++ src/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserPostEntity } from './user/models/post.entity';
-import{ Repository } from 'typeorm';
+import{ Repository, UpdateResult,DeleteResult } from 'typeorm';
 import { UserPost } from './user/models/post.interface';
 import { from, observable, Observable } from 'rxjs';
 
@@ -17,5 +17,13 @@ export class UserService {
 
     findAllUser():Observable <UserPost[]> {
         return from(this.userPostRepository.find());
+    }
+
+    updateUser(id: number, userPost:UserPost): Observable<UpdateResult>{
+        return from(this.userPostRepository.update(id, userPost))
+
+    }
+    deleteUser( id: number): Observable<DeleteResult>{
+        return from(this.userPostRepository.delete(id))
     }
 }
