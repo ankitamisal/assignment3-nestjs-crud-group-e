756c3659fcf185bf1f7e7ecc4f9db17e98f5b99d
swapnil-mirgane <jigarswapnil143@gmail.com>
2022-08-16 09:58:37 +0530

    feat:create module book

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from './user/user.module';
 
 import { StudentManagmentModule } from './student-managment/student-managment.module';
 import { ProductManagementModule } from './product-management/product-management.module';
+import { BookModule } from './book/book.module';
 
 @Module({
 
-  imports: [UserModule, StudentManagmentModule, ProductManagementModule],
+  imports: [UserModule, StudentManagmentModule, ProductManagementModule, BookModule],
 
   controllers: [AppController],
 
