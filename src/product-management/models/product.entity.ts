d44e43d12cabed9529cbeef4bcf5cc8e7429ece7
Amit01 <amitkumar1437786@gmail.com>
2022-08-16 16:26:55 +0530

    feat:demo

--- src/product-management/models/product.entity.ts
+++ src/product-management/models/product.entity.ts
@@ -12,15 +12,15 @@ import { timestamp } from 'rxjs';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 //import {v4 as uuidv4} from 'uuid';
 
-export enum ProductData{
-   AVAILABLE= "Available",
-   OUTOFSTOCK="OutOfStock"
+export enum ProductData {
+  AVAILABLE = "Available",
+  OUTOFSTOCK = "OutOfStock"
 }
-export enum ProductSize{
-    SMALL="S",
-    MEDIUM="M",
-    LARGE="L",
-    EXTRALARGE="XL"
+export enum ProductSize {
+  SMALL = "S",
+  MEDIUM = "M",
+  LARGE = "L",
+  EXTRALARGE = "XL"
 }
 @Entity('product_post')
 
@@ -29,16 +29,16 @@ export class ProductPostEntity {
   @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  @Column({default:''})
+  @Column({ default: '' })
   productName: string;
 
-  @Column({type:"int"})
-  price:number;
+  @Column({ type: "int" })
+  price: number;
 
-  @Column({ type:'enum',enum:ProductData,default:null})
-  stock:ProductData;
+  @Column({ type: 'enum', enum: ProductData, default: null })
+  stock: ProductData;
 
-  @Column({ type:'enum',enum:ProductSize,default:null})
+  @Column({ type: 'enum', enum: ProductSize, default: null })
   size: ProductSize;
 
   @Column({ default: '' })
