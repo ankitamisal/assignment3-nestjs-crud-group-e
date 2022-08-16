0f963c6043444720bdd33054edeabcb14306a5a8
swapnil-mirgane <jigarswapnil143@gmail.com>
2022-08-16 10:34:33 +0530

    feat:entity and interface add

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookEntity } from './entities/book.entity';
 
 @Module({
   controllers: [BookController],
-  providers: [BookService]
+  providers: [BookService],
+  imports:[
+    TypeOrmModule.forFeature([BookEntity])
+  ],
 })
 export class BookModule {}
