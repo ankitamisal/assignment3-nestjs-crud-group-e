cac84aef7920513496f79c190f4d73a1ea25803a
Prathamesh094 <prathamesh.howale@gmail.com>
2022-08-17 15:04:22 +0530

    feat:add validation

--- src/user/models/post.entity.ts
+++ src/user/models/post.entity.ts
@@ -1,17 +1,23 @@
+// import { from } from 'rxjs';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, IsString, IsInt} from 'class-validator';
 
 @Entity('User-Post')
 export class UserPostEntity {
   @PrimaryGeneratedColumn()
+  @IsInt()
   id: number;
 
   @Column()
+  @IsString()
   name: string;
 
   @Column()
+  @IsString()
   State: string;
 
   @Column()
+  @IsNotEmpty()
   body: string;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
