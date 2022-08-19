172350da20927318bd6f6b798c345eb4f173aa51
swapnil-mirgane <jigarswapnil143@gmail.com>
2022-08-19 18:20:25 +0530

    fix:bookController fix image part

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, Res, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express/multer';
 import { ApiBody, getSchemaPath } from '@nestjs/swagger';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
+  imagepath: string;
   constructor(private readonly bookService: BookService) {}
 
   @Post()
@@ -33,5 +37,30 @@ export class BookController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.bookService.deleteBook(+id);
+  }
+
+  @Post('images')
+  @UseInterceptors(FileInterceptor('image',{
+    storage: diskStorage({
+      destination: './image',
+      filename:(req, image, callback) =>{
+        const uniqueSuffix = Date.now() +'-'+ Math.round(Math.random()*1e9);
+        const ext = extname(image.originalname);
+        // const filename = `${image.originalname}-${uniqueSuffix}${ext}`;
+        const filename = `${uniqueSuffix}${ext}`;
+        callback(null,filename);
+      }
+    }),
+  }),
+  )
+  handleupload(@UploadedFile() image:Express.Multer.File){
+    this.imagepath = image.path;
+    console.log('image', image);
+    console.log('path',image.path);
+    return "file upload API";
+}
+@Get('/image/:image')
+  seeUploadedFile(@Param('image') image, @Res() res) {
+    return res.sendFile(image, { root: './image' });
   }
 }
