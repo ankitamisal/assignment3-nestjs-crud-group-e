2c30904e771ca23df94bc2d7ab14edc20f50ca34
ankitamisal <ankitamisal56@gmail.com>
2022-08-23 16:25:53 +0530

    chore:createdto file update

--- src/Employeee/entity/employee.entity.ts
+++ src/Employeee/entity/employee.entity.ts
@@ -1,12 +1,10 @@
-// import { IsNotEmpty } from 'class-validator';
+
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class employee_t {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
-  //@Field({ nullable: true })
-  // @IsNotEmpty()
   Emp_FirstName: string;
   @Column()
   Emp_LastName: string;
@@ -16,6 +14,6 @@ export class employee_t {
   Emp_Email: string;
   @Column()
   Emp_Add: string;
-  @Column()
-  Emp_profile:string;
+  // @Column()
+  // Emp_profile: string;
 }
